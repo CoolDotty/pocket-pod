2021db675b8b0947d2e1a46cab37ad71b2967501
Dot Phillips <dot@x3c.ca>
2026-02-10 20:17:27 -0800

    unneeded user check

--- web/src/pages/Dashboard/index.tsx
+++ web/src/pages/Dashboard/index.tsx
@@ -31,7 +31,6 @@ export default function DashboardPage() {
     ? getPodmanContainersErrorMessage(containersError)
     : null;
 
-  if (!user) return null;
 
   return (
     <section className={styles.card}>
